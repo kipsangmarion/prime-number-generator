6a56a0facbb3724fc01638b598fb91a644b4a995
kipsangmarion <kipsangmarion@gmail.com>
2023-07-26 11:58:04 +0300

    init C-file commit

---
+++ prime_generator.c
@@ -0,0 +1,12 @@
+/**
+ * file: prime_generator.c
+ * author: mrion
+ * description: c code to generate prime numbers upto a certain limit
+*/
+#include <stdio.h>
+
+int main(void)
+{}
+
+int prime(int limit)
+{}
