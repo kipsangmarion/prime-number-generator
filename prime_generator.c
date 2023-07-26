54e348c621a689565459a96646de737dba3bf7ae
kipsangmarion <kipsangmarion@gmail.com>
2023-07-26 12:33:07 +0300

    corrected prime generator code

--- prime_generator.c
+++ prime_generator.c
@@ -7,24 +7,9 @@
 #include <stdbool.h>
 #include <math.h>
 
-int main()
-{
-	int limit;
-	printf("Enter a limit for the generator: ");
-	scanf("%d", &limit);
-	
-	if (limit < 2){
-		printf("There are no prime numbers upto %d.\n", limit);
-		return 0;
-	}
-	
-	primeGen(limit);
-	return0;
-}
-
 int primeGen(int limit)
 {
-	bool IsPrime[limit + 1];
+	bool isPrime[limit + 1];
 	int i, j;
 	
 	// initialize array elements to true
@@ -55,3 +40,20 @@ int primeGen(int limit)
 	}
 	printf("\n");
 }
+
+int main()
+{
+	int limit;
+	printf("Enter a limit for the generator: ");
+	scanf("%d", &limit);
+	
+	if (limit < 2){
+		printf("There are no prime numbers upto %d.\n", limit);
+		return 0;
+	}
+	
+	primeGen(limit);
+	return 0;
+}
+
+
